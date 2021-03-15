e6d708c7239d04dfc529622ac8ce391cb8edb19c
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-15 18:13:04 +0700

    lab6_4 ได้ไม่ครบทุกกรณี

---
+++ lab6_4.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int number[num][2];
+    int cheak[num];
+    int wow[num];
+    for(int i=0;i<num;i++)
+    {
+        cheak[i]=i+1;
+        wow[i]=0;
+    }
+    for(int i=0;i<num;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            scanf("%d",&number[i][j]);
+        }
+    }
+    for(int i=0;i<num;i++)
+    {
+        for(int j=0;j<2;j++)
+        {
+            for(int k=0;k<num;k++)
+            {
+                if(number[i][j]==cheak[k])
+                {
+                    wow[k]++;
+                }
+            }
+        }
+    }
+    int max=wow[0],min=wow[0];
+    int savemax,savemin;
+    for(int t=0;t<num;t++)
+    {
+        if(max<wow[t])
+        {
+            max=wow[t];
+            savemax=t;
+        }
+        if(min>wow[t])
+        {
+            min=wow[t];
+            savemin=t;
+        }
+    }
+    printf("%d %d",savemax+1,savemin+1);
+    return 0;
+}
